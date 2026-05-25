45f03732c6b3c98d29ef947549564eca7e7b8222
Vinícius Johnson <dev.vinicius.j@gmail.com>
2026-05-25 00:58:29 -0400

    Delete refatoração_de_front/O-Impacto-do-uso-Excessivo-das-Telas-na-Vida-Cotidiana directory

--- refatoração_de_front/O-Impacto-do-uso-Excessivo-das-Telas-na-Vida-Cotidiana/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-// Script para futuras funcionalidades interativas da página principal
-const btn_quiz = document.querySelector('#btn_for_quiz');
-btn_quiz.addEventListener('click', () => {
-   return window.location.href = './pages/Quiz.html';
-});
